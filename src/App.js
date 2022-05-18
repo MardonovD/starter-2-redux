b2c61a29f98c478abaec11aa38eb0320e4e0fa89
Dostonbek <mardonovdoston69@gmail.com>
2022-05-18 18:40:15 +0300

    vjf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./style/styles.css";
 function App() {
   const [data, setData] = useState(0);
+  const [toggleBtn, setToogleBtn] = useState(false);
 
   function inc() {
     setData((prev) => prev + 1);
@@ -19,6 +20,13 @@ function App() {
       <button onClick={dec} className="btn btn-danger">
         DECR
       </button>
+      <button
+        onClick={() => setToogleBtn(!toggleBtn)}
+        className="btn btn-secondary"
+      >
+        Toogle Btn
+      </button>
+      {toggleBtn ? <p className="lead"> Youtube Content</p> : null}
     </div>
   );
 }
