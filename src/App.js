21c5101d570303eb00d3a2efa8b9c05bb84c8b82
Dostonbek <mardonovdoston69@gmail.com>
2022-05-18 20:19:46 +0300

    dkc

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import { useState } from "react";
+import TableHeader from "./components/TableHeader";
+import TableItem from "./components/TableItem";
 
 import "./style/styles.css";
 function App() {
   return (
     <div className="app w-50 mx-auto">
       <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Programming Language.</th>
-            <th>Stack</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+        <TableHeader />
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>JavaScript</td>
-            <td>Mern Stack</td>
-            <td>
-              <button className="btn btn-outline-danger">delete</button>
-            </td>
-          </tr>
+          <TableItem />
+          <TableItem />
+          <TableItem />
         </tbody>
       </table>
     </div>
---
+++ src/components/TableItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TableItem = () => {
+  return (
+    <tr>
+      <td>1</td>
+      <td>JavaScript</td>
+      <td>Mern Stack</td>
+      <td>
+        <button className="btn btn-outline-danger">delete</button>
+      </td>
+    </tr>
+  );
+};
+
+export default TableItem;
