a2a6ea80a472ec2291f6ab5424f936d313e11b37
Dostonbek <mardonovdoston69@gmail.com>
2022-05-18 18:11:34 +0300

    jdv

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 function App() {
+  const [data, setData] = useState(0);
+  console.log(data);
+  function inc() {
+    setData((prev) => prev + 1);
+  }
+  function dec() {
+    setData((prev) => prev - 1);
+  }
+
   return (
     <div>
-      <h3>Counet:</h3>
-      <button className="btn btn-success">INCR</button>
-      <button className="btn btn-danger">DECR</button>
+      <h3>Counet: {data}</h3>
+      <button onClick={inc} className="btn btn-success">
+        INCR
+      </button>
+      <button onClick={dec} className="btn btn-danger">
+        DECR
+      </button>
     </div>
   );
 }
