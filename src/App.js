1e8be0c3aa871d26690b32c0620205ecc2743470
Dostonbek <mardonovdoston69@gmail.com>
2022-05-19 08:16:04 +0300

    fdv

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 
 const App = () => {
   let count = 0;
+  function inc() {
+    count += 1;
+    console.log(count);
+  }
+  function dec() {
+    count -= 1;
+    console.log(count);
+  }
   return (
     <>
       <h3>Count:{count}</h3>
-      <button onClick={() => (count += 1)} className="btn btn-success">
+      <button onClick={inc} className="btn btn-success">
         INCR
       </button>
-      <button onClick={() => (count -= 1)} className="btn btn-danger">
+      <button onClick={dec} className="btn btn-danger">
         DECR
       </button>
     </>
